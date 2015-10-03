a99e25bd82e4eb7c5c6566b164ee020fa23b9fec
Maxim Galushka <mgalushka@fb.com>
2015-10-03 22:56:00 +0100

    std::cout and printing std::string

---
+++ hardway/ex2.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::string str = "This is string";
+
+    std::cout << "Length(" << str << ") = " << str.length() << "\n";
+    return 0;
+}
