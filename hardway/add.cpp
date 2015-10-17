678bec42c0f8c701ebdd25ad9f62709e76a5886f
Maxim Galushka <mgalushka@fb.com>
2015-10-17 17:50:57 +0100

    1st task a slight better move

--- hardway/add.cpp
+++ hardway/add.cpp
@@ -5,37 +5,46 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string>
+#include <algorithm>
 
 #ifdef DEBUG
 #else
 #define DEBUG false
 #endif
 
-std::string add(std::string a, std::string b);
-std::string reverse(std::string input);
+using namespace std;
+
+std::string* add(std::string& a, std::string& b);
+std::string& reverse(std::string& input);
 
 int main(){
-    std::cout << add("1", "1") << std::endl;
-    std::cout << add("1", "0") << std::endl;
-    std::cout << add("0", "1") << std::endl;
-    std::cout << add("10", "11") << std::endl;
-    std::cout << add("11", "11") << std::endl;
-    std::cout << add("111", "1111") << std::endl;
-    std::cout << add("1000000000000000000", "1111") << std::endl;
-    std::cout << add("100101010", "101001010") << std::endl;
-    std::cout << reverse("abcdefgh") << std::endl;
+    string a("1");
+    string b("1");
+    std::cout << *add(a, b) << std::endl;
+    //std::cout << add("1", "0") << std::endl;
+    //std::cout << add("0", "1") << std::endl;
+    //std::cout << add("10", "11") << std::endl;
+    //std::cout << add("11", "11") << std::endl;
+    //std::cout << add("111", "1111") << std::endl;
+    //std::cout << add("1000000000000000000", "1111") << std::endl;
+    //std::cout << add("100101010", "101001010") << std::endl;
+
+    std::string input("abcdefgh");
+    auto str = reverse(input);
+
+    std::cout << str << std::endl;
     return 0;
 }
 
 // add 2 binary strings byte-by-byte
-std::string add(std::string a, std::string b) {
-    std::string ar = reverse(a);
-    std::string br = reverse(b);
+std::string* add(std::string& a, std::string& b) {
+    std::string& ar = reverse(a);
+    std::string& br = reverse(b);
 
     uint32_t max_length = std::max(ar.length(), br.length());
 
-    std::string result;
-    result.reserve(max_length);
+    std::string* result = new std::string();
+    result->reserve(max_length);
 
     int carry = 0;
     for(uint32_t i = 0; i < max_length; i++) {
@@ -56,25 +65,20 @@ std::string add(std::string a, std::string b) {
             std::cout << "sum: " << sum << ", bit: " << bit << ", carry: " << carry << std::endl;
         }
 
-        result.insert(0, 1, static_cast<char>(bit + '0'));
+        result->insert(0, 1, static_cast<char>(bit + '0'));
     }
 
     if (carry > 0) {
         if (DEBUG) {
             std::cout << "printing carry" << std::endl;
         }
-        result.insert(0, 1, '1');
+        result->insert(0, 1, '1');
     }
     
     return result;
 }
 
-std::string reverse(std::string input) {
-    std::string reversed;
-    uint32_t L = input.length();
-    reversed.reserve(L);
-    for(uint32_t i = 0; i < L; i++) {
-        reversed.insert(0, 1, input[i]);
-    }
-    return reversed;
+std::string& reverse(std::string& input) {
+    std::reverse(input.begin(), input.end());
+    return input;
 }
