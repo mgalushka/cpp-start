a03a676f3216317156ddc99962fd551ad57ce205
Maxim Galushka <mgalushka@fb.com>
2015-10-18 21:52:57 +0100

    Initial (compiled) implementation for both parts - week 3.

--- hardway/nearest/Nearest.cpp
+++ hardway/nearest/Nearest.cpp
@@ -6,8 +6,24 @@ namespace cpp {
 
     struct ByDistance {
         const Point& origin;
+        const double distanceThreshold;
 
         bool operator()(const Point& lp, const Point& rp) {
+            double ld = dist(origin, lp);
+            double rd = dist(origin, rp);
+            if (ld <= distanceThreshold && ld <= distanceThreshold) {
+                return ld - rd;
+            }
+            if (ld > distanceThreshold && ld <= distanceThreshold) {
+                return -1;
+            }
+            if (ld <= distanceThreshold && ld > distanceThreshold) {
+                return 1;
+            }
+            return 0;
+        }
+
+        double dist(const Point& lp, const Point& rp){
             double xs = (lp.x - rp.x) * (lp.x - rp.x);
             double ys = (lp.y - rp.y) * (lp.y - rp.y);
             double zs = (lp.z - rp.z) * (lp.z - rp.z);
@@ -16,21 +32,22 @@ namespace cpp {
     };
 
     vector<Point> nearestN(const vector<Point>& points, int N) {
-        vector<Point> temp(points);
-        ByDistance distance = {points[0]};
-        sort(temp.begin(), temp.end(), distance);
-
-        vector<Point> result(N);
-        copy_n(temp.begin(), N, result.begin());
-        return result;
+        return nearestN(points, N, points[0], INFINITY);
     }
 
     vector<Point> nearestN(const vector<Point>& points,
                            int N,
                            const Point& reference,
                            double distanceThreshold) {
-        vector<Point> ret;
-        return ret;
+
+
+        vector<Point> temp(points);
+        ByDistance distance = {reference, distanceThreshold};
+        sort(temp.begin(), temp.end(), distance);
+
+        vector<Point> result(N);
+        copy_n(temp.begin(), N, result.begin());
+        return result;
     }
 }
 
