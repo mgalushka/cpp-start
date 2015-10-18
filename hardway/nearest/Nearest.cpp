98e3b7f0ea29e721c284c19b629904f0731c63ce
Maxim Galushka <mgalushka@fb.com>
2015-10-18 21:45:13 +0100

    Homework week 3  - distance

--- hardway/nearest/Nearest.cpp
+++ hardway/nearest/Nearest.cpp
@@ -1,21 +1,39 @@
 #include "Nearest.h"
 
+using namespace std;
+
 namespace cpp {
 
+    struct ByDistance {
+        const Point& origin;
 
-std::vector<Point> nearestN(const std::vector<Point>& points,
-                            int N) {
-  std::vector<Point> ret;
-  return ret;
+        bool operator()(const Point& lp, const Point& rp) {
+            double xs = (lp.x - rp.x) * (lp.x - rp.x);
+            double ys = (lp.y - rp.y) * (lp.y - rp.y);
+            double zs = (lp.z - rp.z) * (lp.z - rp.z);
+            return sqrt(xs + ys + zs);
+        }
+    };
+
+    vector<Point> nearestN(const vector<Point>& points, int N) {
+        vector<Point> temp(points);
+        ByDistance distance = {points[0]};
+        sort(temp.begin(), temp.end(), distance);
+
+        vector<Point> result(N);
+        copy_n(temp.begin(), N, result.begin());
+        return result;
+    }
+
+    vector<Point> nearestN(const vector<Point>& points,
+                           int N,
+                           const Point& reference,
+                           double distanceThreshold) {
+        vector<Point> ret;
+        return ret;
+    }
 }
 
-std::vector<Point> nearestN(const std::vector<Point>& points,
-                            int N,
-                            const Point& reference,
-                            double distanceThreshold) {
-  std::vector<Point> ret;
-  return ret;
-}
-
+int main() {
 
 }
