a73ee7a6a32eedad45dfdec5d64a178ff2a56478
Maxim Galushka <mgalushka@fb.com>
2016-06-06 10:50:32 +0100

    First version is working

--- video/video_phone.c
+++ video/video_phone.c
@@ -3,18 +3,24 @@
 #include "gstreamer_utils.h"
 #include "send_video.h"
 
+/*
+ * To run:
+ * ./video_phone.o 0 $(host -4 -t A stun.stunprotocol.org | awk '{ print $4 }') --gst-debug-level=4
+ */
 int main(int argc, char *argv[]) {
 
   if (argc > 4 || argc < 2 || argv[1][1] != '\0') {
     fprintf(stderr, "Usage: %s 0|1 stun_addr [stun_port]\n", argv[0]);
     return EXIT_FAILURE;
   }
-  controlling = argv[1][0] - '0';
+  gint controlling = argv[1][0] - '0';
   if (controlling != 0 && controlling != 1) {
     fprintf(stderr, "Usage: %s 0|1 stun_addr [stun_port]\n", argv[0]);
     return EXIT_FAILURE;
   }
 
+  gchar *stun_addr;
+  gint stun_port;
   if (argc > 2) {
     stun_addr = argv[2];
     if (argc > 3) {
@@ -29,8 +35,6 @@ int main(int argc, char *argv[]) {
 
   /* Initialize GStreamer */
   gst_init (&argc, &argv);
-
-
 
   return 0;
 }
