81cdfa7c998625b354825e9f68e13275417fbd4b
Maxim Galushka <mgalushka@fb.com>
2016-07-12 12:26:39 +0100

    Checking literal types

--- folly/FollyJson.cpp
+++ folly/FollyJson.cpp
@@ -40,5 +40,9 @@ int main(int, char**) {
     std::cout << "Count(some_field) = " << mergeFields.count("some_field") << std::endl;
     std::cout << "Count(some_field0) = " << mergeFields.count("some_field0") << std::endl;
 
+
+    std::cout << "is_string_literal = " << std::is_literal_type<std::string>::value << std::endl;
+    std::cout << "is_StringPiece_literal = " << std::is_literal_type<folly::StringPiece>::value << std::endl;
+
     return 0;
 }
