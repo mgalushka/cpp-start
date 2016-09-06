7a3d9e847ec7499b311e34cc313512e9a25d189b
Maxim Galushka <mgalushka@fb.com>
2016-09-06 19:28:02 +0530

    multithreading test

---
+++ threads/TwoThreads.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "TwoThreads.h"
+
+namespace threads {
+
+}
+
+int main() {
+  std::cout << "Running" << std::endl;
+
+  // std::thread helper1(foo);
+
+  return 0;
+}
---
+++ threads/TwoThreads.h
@@ -0,0 +1,8 @@
+namespace threads {
+
+class Task {
+
+};
+
+
+}
