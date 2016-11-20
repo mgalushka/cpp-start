6ce65039deb798fb427576b64b2ae34cd9f4b41d
Maxim Galushka <mgalushka@gmail.com>
2016-11-20 12:36:29 +0000

    Passing data to thread by-reference

--- threads/RunThreads.cpp
+++ threads/RunThreads.cpp
@@ -3,7 +3,11 @@
 #include <vector>
 #include <thread>
 
-void first() {
+void first(std::vector<std::string>& values) {
+  values.reserve(100);
+  for (int i = 0; i < 100; ++i) {
+    values.emplace_back(std::to_string(i));
+  }
   std::cout << "Thread 1" << std::endl;
 }
 
@@ -12,10 +16,17 @@ void second() {
 }
 
 int main() {
-  std::thread t1(first);
+  std::vector<std::string> values;
+    
+  std::thread t1(first, std::ref(values));
   std::thread t2(second);
 
   t1.join();
   t2.join();
+  
+  for (auto& value : values) {
+    std::cout << value << std::endl;
+  }
+  
   return 0;
 }
