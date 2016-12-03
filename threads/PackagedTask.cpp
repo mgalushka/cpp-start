e95e21031df725cb4798f52a051303575ee4cbfa
Maxim Galushka <mgalushka@fb.com>
2016-12-03 10:37:25 +0000

    Passing std::packaged_task to be executed by other thread and main thread has future which contains result

---
+++ threads/PackagedTask.cpp
@@ -0,0 +1,34 @@
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+/// want to pass task from 1 tread to another
+std::promise<int32_t> prom;
+
+
+void second(std::packaged_task<int32_t(int32_t)>& task, int32_t x) {
+  std::cout << "Thread 2" << std::endl;
+  task(x);
+}
+
+int32_t square(int32_t a) {
+  return a * a;
+}
+
+
+int main() {
+  using namespace std::chrono_literals;
+  std::packaged_task<int32_t(int32_t)> task(&square);
+
+  std::future<int32_t> fut = task.get_future();
+  std::thread t1(second, std::ref(task), 100);
+  std::this_thread::sleep_for(300ms);
+
+  std::cout << "Scuare result: " << fut.get() << std::endl;
+
+  t1.join();
+  return 0;
+}
