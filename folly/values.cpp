ebd7590e0086850129566f3550df4c5576b39b20
Maxim Galushka <mgalushka@gmail.com>
2017-08-19 20:14:49 +0100

    Checking how lvalues actually work. Proved that ternary expression itself is lvalue.

---
+++ folly/values.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+int main () {
+
+  int a = 0;
+  int b = 0;
+  int c = 0;  
+  
+  a == 3 ? b : c = 17;
+  
+  std::cout << " a = " << a << std::endl;
+  std::cout << " b = " << b << std::endl;
+  std::cout << " c = " << c << std::endl;
+  
+  return 0;
+}
